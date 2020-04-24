214620781a0e1e97cf5a2a572bf0edd4b1865e59
fxf111 <fxf12345@139.com>
2020-04-24 19:28:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const OSS = require('ali-oss');
-const fs = require('fs');
-const { resolve } = require('path');
+const ossdir = require('ali-oss-dir');
+
 
 (async () => {
   try {
@@ -14,31 +14,14 @@ const { resolve } = require('path');
       bucket: core.getInput('bucket')
     })
 
-    const assetPath = core.getInput('asset-path', { required: true }).replace(/\/+$/g, '')
-    const targetPath = core.getInput('target-path', { required: true }).replace(/\/+$/g, '')
+    const assetPath = core.getInput('asset-path', { required: true })
+    const targetPath = core.getInput('target-path', { required: true })
 
-    if (fs.existsSync(assetPath)) {
-      const stat = fs.lstatSync(assetPath)
-      // upload file
-      if (stat.isFile()) {
-        const filename = assetPath.slice(assetPath.lastIndexOf('/') + 1)
-        const res = await oss.put(targetPath, assetPath)
-        core.setOutput('url', res.url)
-      }
-      // upload dir
-      if (stat.isDirectory()) {
-        const files = fs.readdirSync(resolve(assetPath))
-        const res = await Promise.all(
-          files.filter(f => fs.lstatSync(resolve(assetPath, f)).isFile())
-            .map(file => {
-              return oss.put(`${targetPath}/${file}`, resolve(assetPath, file))
-            })
-        )
-        core.setOutput('url', res.map(r => r.url).join(','))
-      }
-    } else {
-      core.setFailed('asset_path does not exist.')
-    }
+    ossdir(oss).upload(assetPath).to(targetPath).then((results) => {
+      console.log(results);
+    });
+
+   
   } catch (err) {
     core.setFailed(err.message)
   }
